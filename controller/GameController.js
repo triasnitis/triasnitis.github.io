6c2a7cf16816feb4fca727d262763b32f0a8f874
triaslazuardi <trias.lazuardi@gmail.com>
2023-01-04 09:40:00 +0700

    add 1 game

---
+++ controller/GameController.js
@@ -0,0 +1,8 @@
+function changeToGame(_indexGame) {
+    console.log("test :" + _indexGame)
+    switch(_indexGame) {
+        case 1:
+            window.location.assign("../game/gamematch/index.html");
+            break
+    }
+}
